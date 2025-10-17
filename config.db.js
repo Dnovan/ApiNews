064e0ffd214b99fc7037b59a06ec57cf27db8ffb
Dnovan <deicazadonovan@gmail.com>
2025-10-17 16:31:48 -0600

    feat: Add DB_PORT configuration

--- config.db.js
+++ config.db.js
@@ -1,21 +1,23 @@
 const Sequelize = require('sequelize');
-require('dotenv').config(); 
+require('dotenv').config();
 
-const { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER } = require('./config.js');
 
+const { DB_HOST, DB_NAME, DB_PASSWORD, DB_USER, DB_PORT } = require('./config.js');
 
 const connection = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
     host: DB_HOST,
     dialect: 'mysql',
+    port: DB_PORT 
 });
 
 
 connection.authenticate()
     .then(() => {
-        console.log('Se ha establecido conexión con la base de datos');
+        console.log('Se ha establecido conexión con la base de datos exitosamente.');
     })
     .catch(err => {
-        console.log('No se pudo establecer conexión con la base de datos');
+        console.error('Error: No se pudo establecer conexión con la base de datos.');
+        console.error('Detalles del error:', err.name, err.message);
     });
 
 module.exports = { connection };
